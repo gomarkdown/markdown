87d7df2d203e25ed3d4dcdf939238905a6c81509
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2018-05-06 20:38:49 -0700

    simplify test_with_codecoverage.sh

--- s/test_with_codecoverage.sh
+++ s/test_with_codecoverage.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-# based on https://github.com/ory/go-acc/blob/master/README.md
+# This requires 1.10+ to support -coverprofile with -coverpkg
 
 set -e
 
+# List of packages to test
+pkgs=$(go list ./... | grep -v /vendor)
+# list of packages in a format suitable for -coverpkg
 cover_pkgs=$(go list ./... | grep -v /vendor | tr "\n" ",")
-echo $cover_pkgs
 
-echo 'mode: atomic' > coverage.txt
-go list ./... | grep -v /vendor | grep -v /cmd | xargs -n1 -I{} sh -c 'go test -race -covermode=atomic -coverprofile=coverage.tmp -coverpkg ${cover_pkgs} {} && tail -n +2 coverage.tmp >> coverage.txt || exit 255' && rm coverage.tmp
+go test -race -covermode=atomic -coverprofile=coverage.txt -coverpkg ${cover_pkgs} ${pkgs}
