637a45dd3fa7d51dea9d4a315ce85981b6a3c100
Miek Gieben <miek@miek.nl>
2018-08-08 21:09:55 +0100

    Add codeinclude test
    Add test for code include <{{..}}. And Fix off-by-one error if we
    process a code include.
    
    Also set the language if we have a code include.
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- parser/include_test.go
+++ parser/include_test.go
@@ -70,3 +70,35 @@ func TestIsInclude(t *testing.T) {
 		}
 	}
 }
+
+func TestIsCodeInclude(t *testing.T) {
+	tests := []struct {
+		data []byte
+		file string
+		addr string
+		read int
+	}{
+		{
+			[]byte("<{{foo}}"),
+			"foo", "", 8,
+		},
+		{
+			[]byte("<{{foo}}  "),
+			"foo", "", 8,
+		},
+	}
+
+	p := New()
+	for i, test := range tests {
+		file, addr, read := p.isInclude(test.data)
+		if file != test.file {
+			t.Errorf("test %d, want %s, got %s", i, test.file, file)
+		}
+		if string(addr) != test.addr {
+			t.Errorf("test %d, want %s, got %s", i, test.addr, addr)
+		}
+		if read != test.read {
+			t.Errorf("test %d, want %d, got %d", i, test.read, read)
+		}
+	}
+}
