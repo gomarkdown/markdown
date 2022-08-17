a952210ff206c3045bee92eaefdcb378ca6c4744
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2022-08-17 22:38:58 +0000

    export parser.IsSafeURL (fixes #254)

--- html/renderer.go
+++ html/renderer.go
@@ -11,7 +11,6 @@ import (
 	"strings"
 
 	"github.com/gomarkdown/markdown/ast"
-	"github.com/gomarkdown/markdown/internal/valid"
 	"github.com/gomarkdown/markdown/parser"
 )
 
@@ -293,7 +292,7 @@ func needSkipLink(r *Renderer, dest []byte) bool {
 	}
 	isSafeURL := r.IsSafeURLOverride
 	if isSafeURL == nil {
-		isSafeURL = valid.IsSafeURL
+		isSafeURL = parser.IsSafeURL
 	}
 	return flags&Safelink != 0 && !isSafeURL(dest) && !isMailto(dest)
 }
@@ -1269,33 +1268,6 @@ func slugify(in []byte) []byte {
 		}
 	}
 	return out[a : b+1]
-}
-
-// TODO: move to internal package
-// isAlnum returns true if c is a digit or letter
-// TODO: check when this is looking for ASCII alnum and when it should use unicode
-func isAlnum(c byte) bool {
-	return (c >= '0' && c <= '9') || isLetter(c)
-}
-
-// isSpace returns true if c is a white-space charactr
-func isSpace(c byte) bool {
-	return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == '\v'
-}
-
-// isLetter returns true if c is ascii letter
-func isLetter(c byte) bool {
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-}
-
-// isPunctuation returns true if c is a punctuation symbol.
-func isPunctuation(c byte) bool {
-	for _, r := range []byte("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~") {
-		if c == r {
-			return true
-		}
-	}
-	return false
 }
 
 // BlockAttrs takes a node and checks if it has block level attributes set. If so it
