6bc1ffdc6e8c00d315151dead024d04d45421384
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-11-02 16:10:59 +0100

    change parsing of code blocks ```go meta (fixex #320)
    Before CodeBlock.Info would be "go" and "meta" was part of
    CodeBlock.Literal
    With this change CodeBlock.Info is "go meta"

--- parser/block_test.go
+++ parser/block_test.go
@@ -37,6 +37,30 @@ func TestSVG(t *testing.T) {
 	}
 }
 
+/*
+When we had code block:
+
+```go main.go
+package main
+```
+
+Originally it was:      CodeBlock.Info = "go"         (up to space)
+For #320 changed it to: CodeBlock.Info = "go main.go" (up to newline)
+*/
+func TestCodeBlocIssue320(t *testing.T) {
+	input := "```go main.go,readonly\npackage main\n```"
+	p := NewWithExtensions(CommonExtensions)
+	doc := p.Parse([]byte(input))
+	var buf bytes.Buffer
+	ast.Print(&buf, doc)
+	got := buf.String()
+	exp := "CodeBlock:go main.go,readonly 'package main\\n'\n"
+	if got != exp {
+		t.Errorf("\nInput   [%#v]\nExpected[%#v]\nGot     [%#v]\n",
+			input, exp, got)
+	}
+}
+
 // The RECT element on its own is nothing special, so Markdown is parsed.
 func TestRect(t *testing.T) {
 	input := "<rect> *emph* </rect>\n"
